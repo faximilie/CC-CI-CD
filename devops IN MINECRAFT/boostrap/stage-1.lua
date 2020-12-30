c5eb3592a38d0157e80a3783b95e465f6984a80c
Faximilie <github@faximili.me>
2020-12-30 23:38:22 +1100

    Move function above call

--- devops IN MINECRAFT/boostrap/stage-1.lua
+++ devops IN MINECRAFT/boostrap/stage-1.lua
@@ -2,26 +2,11 @@
 -- Run them
 
 -- Files start with / to make absolute file paths cleaner to implement
-files = {
+local files = {
   '/bootstrap/stage-2.lua',
   '/libs/base64.lua',
   '/libs/json.lua'
 }
-
-gitUrl = 'https://raw.githubusercontent.com/faximilie/CC-CI-CD/main/devops%20IN%20MINECRAFT'
-
-for file in files do
-  url = gitUrl .. file
-  downloadFile(url, file)
-end
-
--- We need to move our current program to it's proper place but we may not know it's name
-fs.move(shell.getRunningProgram(), "/bootstrap/stage-1.lua")
-
--- We can't really fork but we can open a new tab and kill the current one
-shell.openTab("/bootstrap/stage-2.lua", ...) -- This will pass args to the next stage
-shell.exit() -- Close this shell to make sure the new tab is the only tab
-
 
 local function downloadFile(url, fileName)
   local request, err = http.get(url)
@@ -35,5 +20,18 @@ local function downloadFile(url, fileName)
 end
 
 
+local gitUrl = 'https://raw.githubusercontent.com/faximilie/CC-CI-CD/main/devops%20IN%20MINECRAFT'
+
+for _,file in pairs(files) do
+  local url = gitUrl .. file
+  downloadFile(url, file)
+end
+
+-- We need to move our current program to it's proper place but we may not know it's name
+fs.move(shell.getRunningProgram(), "/bootstrap/stage-1.lua")
+
+-- We can't really fork but we can open a new tab and kill the current one
+shell.openTab("/bootstrap/stage-2.lua", ...) -- This will pass args to the next stage
+shell.exit() -- Close this shell to make sure the new tab is the only tab
 
 
