f97d26f62be855e5a5079844b48f91860ac57367
Faximilie <github@faximili.me>
2020-12-30 23:01:02 +1100

    Move bootstrap stages to their own folder

---
+++ devops IN MINECRAFT/boostrap/stage-1.lua
@@ -0,0 +1,35 @@
+-- Pull stage-2.lua and required libraries
+-- Run them
+
+files = {
+  'bootstrap/stage-2.lua',
+  'libs/base64.lua',
+  'libs/json.lua'
+}
+
+git_url = 'https://raw.githubusercontent.com/faximilie/CC-CI-CD/main/devops%20IN%20MINECRAFT/'
+
+for file in files do
+  url = git_url . file
+  download_file(url)
+end
+
+
+local function download_file(url)
+  local request, err = http.get(url)
+
+  if not request then error(err) end
+
+  local split = string.split(url, '/')
+
+  local file_name = split[#split]
+
+  local file = fs.open(file_name, "w")
+  file.write(request.readAll())
+  file.close()
+  request.close()
+end
+
+
+
+
