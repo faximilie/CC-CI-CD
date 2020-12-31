973611713a3975cf05707e1c681b4c2231245c08
Faximilie <github@faximili.me>
2021-01-01 02:45:17 +1100

    Add download path

--- bootstrap/stage-2.lua
+++ bootstrap/stage-2.lua
@@ -10,7 +10,7 @@ local function walkDir(url)
     if v['type'] == 'dir' then
       walkDir(v['url'])
     else
-      common.downloadFile(v['url'], v['path'])
+      common.downloadFile(v['download_url'], v['path'])
     end
   end
 end
@@ -20,6 +20,6 @@ end
 local url = 'https://api.github.com/repos/faximilie/CC-CI-CD/contents?ref='
 local ref = args[2]
 
+-- TODO Deal with ref being nil
 walkDir(url .. ref)
 
--- common.http_get_json_decode('wew')
