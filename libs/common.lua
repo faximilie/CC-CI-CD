762e8bd371aa065558694d0285f4409b1f4ff170
Faximilie <github@faximili.me>
2021-01-08 09:36:13 +1100

    Add auth support

--- libs/common.lua
+++ libs/common.lua
@@ -1,9 +1,14 @@
 -- Bunch of common functions I really don't want to litter the top of my files with
 local common = {}
-function common.httpGetJsonDecode(url)
-  local request, err = http.get(url)
+function common.httpGetJsonDecode(url, auth)
+  local headers = {}
+  local auth = auth or nil
+  if auth then
+    headers['Authorization'] = auth
+  end
+  local request, err = http.get(url, headers)
   if not request then error(err) end
-  output = json.decode(request.readAll())
+  local output = json.decode(request.readAll())
   request.close()
   return output
 end
@@ -19,32 +24,23 @@ function common.downloadFile(url, fileName)
   request.close()
 end
 
-function common.downloadGitB64(gitUrl, fileName)
-  local git = common.httpGetJsonDecode(gitUrl)
+function common.downloadGitB64(gitUrl, fileName, auth)
+  local auth = auth or nil
+  local git = common.httpGetJsonDecode(gitUrl, auth)
   local content = base64.decode(git['content'])
   local file = fs.open(fileName, "w")
   file.write(content)
   file.close()
 end
 
-function common.walkDir(url)
+function common.walkDir(url, prefix, auth)
+  local auth = auth or nil
   local dirContents = common.httpGetJsonDecode(url)
   for _,v in pairs(dirContents) do
     if v['type'] == 'dir' then
-      walkDir(v['url'])
+      common.walkDir(v['url'], prefix, auth)
     else
-      common.downloadGitB64(v['git_url'], v['path'])
-    end
-  end
-end
-
-function common.walkDir(url)
-  local dirContents = common.httpGetJsonDecode(url)
-  for _,v in pairs(dirContents) do
-    if v['type'] == 'dir' then
-      common.walkDir(v['url'])
-    else
-      common.downloadGitB64(v['git_url'], v['path'])
+      common.downloadGitB64(v['git_url'], prefix .. v['path'], auth)
     end
   end
 end
