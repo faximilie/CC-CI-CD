1e2afa34d25bbe941ac5d95690bd3d91134c623c
Faximilie <github@faximili.me>
2021-01-08 09:37:11 +1100

    Add basic functions to git

--- git.lua
+++ git.lua
@@ -1,20 +1,28 @@
 -- Clone entire git repo
 -- Allow branches/versions
 -- Provide mechanisms to update against repo (os.timers?)
-json = require "json"
+json = require "/libs/json"
+base64 = require "/libs/base64"
+common = require "/libs/common"
 
 local arg = { ... }
 
-url = arg[1]
-
-
-
-local function http_get_json_decode(url)
-  local request, err = http.get(url)
-  if not request then error(err) end
-  output = json.decode(request.readAll())
-  request.close()
-  return output
+function addRefToGitUrl(url, ref)
 end
 
-print(http_to_json('https://api.github.com/repos/faximilie/CC-CI-CD/contents/'))
+url = arg[1]
+ref = arg[2]
+
+if ref == nil then
+  ref = 'main'
+end
+
+config = json.decode(common.readFile('config.json'))
+
+if not config['auth_token'] then
+  print('Auth token not defined, Githubs API limit is 60 for un-authenticated users')
+end
+
+gitUrl = 'https://api.github.com/repos/%s/contents?ref=%s'
+
+common.walkDir(gitUrl:format(url, ref), config['auth_token'])
