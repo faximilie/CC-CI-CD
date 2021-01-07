ff37b7d1507099169bbafaee2ee0d81904087285
Faximilie <github@faximili.me>
2021-01-08 09:35:40 +1100

    Ensure stage2 uses main ref

--- bootstrap/stage-2.lua
+++ bootstrap/stage-2.lua
@@ -4,9 +4,7 @@ common = require "/libs/common"
 
 local args = {...}
 
-local url = 'https://api.github.com/repos/faximilie/CC-CI-CD/contents?ref='
-local ref = args[2]
+local url = 'https://api.github.com/repos/faximilie/CC-CI-CD/contents?ref=main'
 
--- TODO Deal with ref being nil
-common.walkDir(url .. ref)
+common.walkDir(url)
 
